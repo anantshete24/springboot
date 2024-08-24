19f2f79b0242587829a3d481c49e081407e25457
anantshete <49448008+anantshete24@users.noreply.github.com>
2024-08-24 05:00:53 -0700

    Add files via upload

---
+++ actuator-example/src/main/java/com/learn/helper/MyDBHealthService.java
@@ -0,0 +1,28 @@
+package com.learn.helper;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyDBHealthService implements HealthIndicator{
+	
+	public static final String DB_SERVICE="Database Service";
+
+	public boolean isHealthGood()
+	{
+		//custom logic 
+		return true;
+	}
+	@Override
+	public Health health() {
+		
+		if(isHealthGood())
+		{
+			return Health.up().withDetail(DB_SERVICE, "Database service is running").build();
+			
+		}
+		return Health.down().withDetail(DB_SERVICE, "Database service is down").build();
+	}
+
+}
---
+++ actuator-example/src/main/java/com/learn/helper/Student.java
@@ -0,0 +1,14 @@
+package com.learn.helper;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+	
+	public Student()
+	{
+		System.out.println("Creating the object of student");
+	}
+	
+}
