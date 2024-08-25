76f161a9e3501c41376ebc5ef66bff8e67d50a07
anantshete <49448008+anantshete24@users.noreply.github.com>
2024-08-24 22:23:39 -0700

    Add files via upload

---
+++ SpringSecurityTest3/src/main/java/com/spring/security/test/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.spring.security.test.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService()
+	{
+		
+		UserDetails normalUser =User.withUsername("anant")
+				.password(passwordEncoder().encode("anant"))
+				.roles("NORMAL")
+				.build();
+		
+		UserDetails adminUser=User.withUsername("admin")
+				.password(passwordEncoder().encode("admin"))
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(normalUser,adminUser);
+	
+	}
+
+	@SuppressWarnings("removal")
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception
+	{
+		httpSecurity.csrf().disable()
+		.authorizeHttpRequests()
+		
+		//.requestMatchers("/home/admin")
+		//.hasRole("ADMIN")
+		//.requestMatchers("/home/normal")
+		//.hasRole("NORMAL")
+		.requestMatchers("/home/public")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.formLogin();
+		return httpSecurity.build();
+		
+	}
+}
---
+++ SpringSecurityTest3/src/test/java/com/spring/security/test/SpringSecurityTest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTest3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
